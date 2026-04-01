6dcc9dbf25fe37a54c9506484b350e1a7ba50274
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-04-01 20:51:25 +0600

    Cart Section designed

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -18,17 +18,11 @@ const ProductCard = ({product}) => {
                 </div>
                 )}
                 
-                {/* <div className='flex justify-start items-center gap-2'>
-                    <FaCheck className='text-green-400 text-sm'/>
-                    <p className='text-sm text-gray-500'>50+ writing templates</p>
-                </div>
-                <div className='flex justify-start items-center gap-2'>
-                    <FaCheck className='text-green-400 text-sm'/>
-                    <p className='text-sm text-gray-500'>Grammar checker</p>
-                </div> */}
-                
             </div>
-            <div className='absolute rounded-full px-3 py-2 top-2 right-2 text-sm text-orange-600 bg-[#FEF3C6]'>
+            <div className={`absolute rounded-full px-3 py-1 top-2 right-2 text-sm
+                 ${product.tagType === "best seller"? "text-orange-600 bg-[#FEF3C6]"
+                    : product.tagType === "popular"? "text-purple-600 bg-[#E1E7FF]"
+                    : "text-green-600 bg-[#DBFCE7]" } `}>
                 <h2>{product.tag}</h2>
             </div>
             <button className='btn btn-primary mt-4 border-none bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full'>Buy Now</button>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,7 @@
 import React, { use } from 'react';
 
 import ProductCard from '../ProductCard/ProductCard';
+import logo from "../../assets/products/writing_23274001.png"
 
 
 const Products = ({productPromise}) => {
@@ -8,7 +9,7 @@ const Products = ({productPromise}) => {
     return (
         <div>
             <div className='w-9/12 mx-auto py-25'>
-                <h2 className='text-4xl font-bold text-center'>Premium Digital Tools</h2>
+                <h2 className='text-3xl md:text-4xl font-bold text-center'>Premium Digital Tools</h2>
                 <p className='text-sm text-gray-400 text-center my-4'>Choose from our curated collection of premium digital products designed <br /> to boost your productivity and creativity.</p>
                 <div className='flex justify-center items-center mb-10'>
                     <div className='flex justify-between items-center shadow-sm rounded-full p-1 gap-5'>
@@ -17,11 +18,36 @@ const Products = ({productPromise}) => {
                     </div>
                 </div>
 
-                {/* Products Container */}
-                <div className='grid grid-cols-3 gap-10'>
-                    {/* Products Card */}
+                {/* Products Container Section*/}
+                <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-10'>
                     {products.map(product => <ProductCard key={product.id} product={product}></ProductCard>)}
-                    
+                </div>
+
+                {/* Products Cart Section */}
+                <div className='shadow-sm rounded-2xl p-10'>
+                    <h2 className='text-2xl font-bold mb-4'>Your Cart</h2>
+                    {/* Cart Card */}
+                    <div className='flex justify-between items-center gap-4 bg-[#F9FAFC] border-none rounded-2xl p-5'>
+                        <div className=' flex justify-between items-center gap-3'>
+                            <div className='w-12 h-12 p-2 rounded-full bg-white'>
+                                <img className='w-full object-contain' src={logo} alt="" />
+                            </div>
+                            <div>
+                                <h2 className='md:text-lg font-semibold'>AI Writing Pro</h2>
+                                <p className=' text-gray-400'>$<span>29</span> </p>
+                            </div>
+                        </div>
+                        <h2 className='text-red-400 font-semibold cursor-pointer'>Remove</h2>
+                    </div>
+
+                    <div className='flex justify-between items-center gap-4 my-6'>
+                        <p className='text-gray-400'>Total:</p>
+                        <h2 className='font-semibold text-xl'>$<span>78</span> </h2>
+                    </div>
+
+                    <button className='btn btn-primary border-none bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full w-full'>Proceed to Checkout</button>
+
+
                 </div>
 
 
